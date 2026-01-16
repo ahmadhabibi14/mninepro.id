7a259fbfe7a40c0663f5da67ac5ad4c347f6a084
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2026-01-16 14:29:31 +0800

    [feat] set to under construction mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,8 @@
 	import { CURRENT_PATH } from '@/states/page';
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
+	import { UNDER_CONSTRUCTION } from '@/states/setting';
+	import Head from '@/partials/Head.svelte';
 	
 	let { children } = $props();
 
@@ -32,4 +34,30 @@
   });
 </script>
 
-{@render children()}
+{#if UNDER_CONSTRUCTION}
+	<Head
+		title="MNine Production | Under Construction"
+	/>
+	
+	<div class="bg-white text-slate-800 top-0 bottom-0 left-0 right-0 h-screen w-screen overflow-x-hidden">
+	<div class="w-full h-full flex flex-col">
+		<main class="flex flex-col left-0 right-0 h-full mx-5">
+			<div class="flex flex-col gap-5 justify-center items-center h-full w-full">
+				<img
+					src="/img/allen_heath_sq_6.jpg"
+					alt="Allen & Heath SQ-6"
+					class="w-full md:w-96 h-auto"
+				/>
+				<div class="flex flex-col gap-2 justify-center items-center text-center">
+					<h1 class="text-mnine-2 font-bold text-3xl md:text-4xl">Under Construction</h1>
+					<p class="md:text-lg text-base">
+						Complete Event Equipment Rental Solutions by <a href="https://mninepro.id" class="italic underline">mninepro.id</a>
+					</p>
+				</div>
+			</div>
+		</main>
+	</div>
+</div>
+{:else}
+	{@render children()}
+{/if}
