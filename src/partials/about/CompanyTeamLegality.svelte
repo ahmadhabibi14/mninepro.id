5b5208adc2538ea527a8c478ec7e15dbbb0dfe7c
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2026-01-23 00:05:12 +0800

    [feat] page /about

---
+++ src/partials/about/CompanyTeamLegality.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  const organizationStructures: {
+    name: string;
+    position: string;
+  }[] = [
+    {
+      name: 'Puguh',
+      position: 'CEO'
+    },
+    {
+      name: 'Kafi',
+      position: 'Project Manager'
+    },
+    {
+      name: 'Yudi',
+      position: 'Operational Manager'
+    },
+    {
+      name: 'Ekik',
+      position: 'Marketing'
+    }
+  ]
+</script>
+
+<div class="md:container w-full px-5 md:px-0 mx-auto md:my-14 my-7">
+  <div class="md:grid md:grid-cols-2 gap-7 flex flex-col">
+    <div class="flex md:flex-row flex-col gap-6 justify-evenly items-center">
+      <h2 class="text-4xl font-orbitron font-bold">Our Team</h2>
+      <div class="flex flex-col md:gap-4 gap-3 justify-center items-center">
+        {#each organizationStructures as os, idx}
+          <div class="bg-black rounded-xl py-3 px-14 text-white flex flex-col gap-1 justify-center items-center">
+            <span class="text-xl md:text-2xl font-bold">{os.name}</span>
+            <span class="w-full h-px bg-slate-300"></span>
+            <span class="font-orbitron">{os.position}</span>
+          </div>
+          {#if idx !== organizationStructures.length - 1}
+            <div class="w-1 bg-mnine md:h-12 h-7 rounded-full"></div>
+          {/if}
+        {/each}
+      </div>
+    </div>
+    <div class="bg-slate-100 rounded-xl md:px-10 px-6 py-7 flex flex-col justify-center items-center gap-9">
+      <h2 class="text-4xl font-orbitron font-bold text-center">Legality</h2>
+      <div class="flex flex-col justify-center text-center gap-5">
+        <div class="flex flex-col gap-0.5">
+          <span>
+            <p>Hj. Indah Purwanti, SH.</p>
+            <p>(Nomor 142 Tgl 25 Maret 2011)</p>
+          </span>
+          <span class="w-52 h-px bg-mnine mx-auto"></span>
+          <span class="font-orbitron text-sm">Akta Notaris</span>
+        </div>
+        <div class="flex flex-col gap-0.5">
+          <span>231 221 000 700 2</span>
+          <span class="w-32 h-px bg-mnine mx-auto"></span>
+          <span class="font-orbitron text-sm">NIB</span>
+        </div>
+        <div class="flex flex-col gap-0.5">
+          <span>03.108.719.0-911.000</span>
+          <span class="w-36 h-px bg-mnine mx-auto"></span>
+          <span class="font-orbitron text-sm">NPWP</span>
+        </div>
+      </div>
+      <div>
+        <img
+          src="/icons/cv-media-sembilan.png"
+          alt="CV. Media Sembilan"
+          class="w-80 h-auto"
+        />
+      </div>
+    </div>
+  </div>
+</div>
--- src/routes/(app)/about/+page.svelte
+++ src/routes/(app)/about/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import CompanyTeamLegality from "@/partials/about/CompanyTeamLegality.svelte";
 	import Head from "@/partials/Head.svelte";
 
 
@@ -8,6 +9,50 @@
   title="Tentang Kami"
 />
 
-<div class="min-h-[900px] w-full flex flex-col gap-7 md:gap-10">
-  tentang
+<div class="min-h-[900px] w-full flex flex-col gap-7 md:gap-10 pb-10">
+  <div class="relative min-h-[200px] md:min-h-[300px] z-1 bg-black" role="banner">
+    <div class="absolute w-full h-full bg-black/60 backdrop-blur-[2px] z-3"></div>
+    <img
+      src="/img/lighting-concert-confetti.png"
+      alt="Concert, pink confetti"
+      class="absolute w-full h-full object-cover z-2"
+    />
+    <div class="absolute w-full h-full flex flex-col z-3 md:mt-32 justify-center md:justify-start">
+      <div class="md:container mx-auto">
+        <div class="w-full flex flex-col text-center gap-6 px-5 md:px-0">
+          <h1 class="text-white font-orbitron leading-8 text-4xl md:text-7xl font-bold">Tentang Kami</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="md:container w-full px-5 md:px-0 mx-auto md:my-14 my-7">
+    <div class="md:grid md:grid-cols-[auto_30%] gap-7 flex flex-col">
+      <div class="flex flex-col md:gap-7 gap-4 h-auto">
+        <p class="text-lg">
+          Kami adalah perusahaan jasa Event Equipment yang telah berdiri sejak tahun 2010 dan berpengalaman dalam melayani berbagai event, baik berskala daerah maupun nasional. Kami menyediakan beragam kebutuhan Event Equipment dengan standar kualitas yang terjaga.
+        </p>
+        <p class="text-lg">
+          Pengalaman panjang di bidang ini menjadi komitmen kami dalam memastikan setiap event yang kami tangani berjalan dengan kualitas terbaik. Kami selalu mengutamakan pelayanan profesional serta menjaga kepercayaan klien sebagai nilai utama dalam setiap kerja sama.
+        </p>
+        <p class="text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam voluptatibus velit perferendis labore harum dolorem corrupti. Voluptates veniam adipisci odit architecto amet, nulla, facere laudantium provident dolore, nobis quisquam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda vitae quae debitis pariatur nemo rerum facere. Quis cumque id, quia perspiciatis laborum error quidem impedit facilis ducimus obcaecati. Veniam, commodi.
+        </p>
+      </div>
+      <div class="grid grid-rows-[auto_65%] gap-6">
+        <div class="flex flex-col justify-center items-center bg-black text-white rounded-xl py-4 md:py-0">
+          <span class="text-2xl font-bold font-orbitron">15+ Tahun</span>
+          <p>Pengalaman</p>
+        </div>
+        <div class="w-full h-auto overflow-hidden rounded-xl">
+          <img
+            src="/img/sound-system-2.jpg"
+            alt="Concerts"
+            class="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+  <CompanyTeamLegality />
 </div>
